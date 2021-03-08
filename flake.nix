eca917b17352728914f35fde288e1f4a2cc82272
Patrik Stutz <patrik.stutz@gmail.com>
2021-03-08 23:51:16 +0100

    use flake-utils

--- flake.nix
+++ flake.nix
@@ -2,21 +2,24 @@
   description = "A command line utility to manage vscode extensions with nix";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.npmlock2nix.url = "github:VanCoding/npmlock2nix";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, npmlock2nix }:
-    let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-      };
-      package = (import npmlock2nix {inherit pkgs;}).build {
-        src = ./.;
-        installPhase = "mkdir $out && cp -r lib $out/lib && cp -r bin $out/bin && cp -r node_modules $out/node_modules"; # mandatory
-      };
-    in
-      rec {
-        packages.x86_64-linux.nvem = package;
-        defaultPackage.x86_64 = package;
-        apps.x86_64-linux.nvem = { type = "app"; program = "${package}/bin/nvem";};
-        defaultApp.x86_64-linux = apps.x86_64-linux.nvem;
-      };
+  outputs = { self, nixpkgs, npmlock2nix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        package = (import npmlock2nix {inherit pkgs;}).build {
+          src = ./.;
+          installPhase = "mkdir $out && cp -r lib $out/lib && cp -r bin $out/bin && cp -r node_modules $out/node_modules"; # mandatory
+        };
+      in
+        rec {
+          packages.nvem = package;
+          defaultPackage = package;
+          apps.nvem = { type = "app"; program = "${package}/bin/nvem";};
+          defaultApp= apps.nvem;
+        }
+    );
 }
